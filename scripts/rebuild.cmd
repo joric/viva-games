459054787892dbe99ce59ae0d408b75e14426deb
joric <joric@users.sourceforge.net>
2023-08-05 23:48:26 +0500

    updated files

--- scripts/rebuild.cmd
+++ scripts/rebuild.cmd
@@ -1,7 +1,5 @@
 python image_mosaic.py
-python parse_names.py
-copy names.js ..\tiles\names.js
-
 python gentiles.py -t jpg -w 512 output.png 1-5 ../tiles
 python gentiles.py -t png -w 512 output.png 6 ../tiles
-
+python parse_names.py
+copy names.js ..\tiles\names.js
