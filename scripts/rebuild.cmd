577c6c9c2247029c49c142db252bf7fc485e488c
joric <joric@users.sourceforge.net>
2023-08-06 21:19:04 +0500

    updated files

--- scripts/rebuild.cmd
+++ scripts/rebuild.cmd
@@ -1,4 +1,4 @@
 python image_mosaic.py
 python gentiles.py -t jpg -w 512 output.png 1-5 ../tiles
 python gentiles.py -t png -w 512 output.png 6 ../tiles
-python copy_settings.py
+python copy_conf.py
