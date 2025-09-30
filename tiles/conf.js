54b49cebffe31813d2c1a2bbfe5749097fd4d874
joric <joric@users.noreply.github.com>
2025-09-30 07:38:50 +0500

    updated files

--- tiles/conf.js
+++ tiles/conf.js
@@ -1,7 +1,7 @@
 var conf=
 {
   "title": "Joric's Viva Games",
-  "attribution": "<a href=\"https://github.com/joric/viva-games\">Joric's Viva Games</a>",
+  "attribution": "<a href=\"https://viva-games.ru\">Viva Games</a>",
   "urlPrefix": "https://viva-games.ru/game/",
   "tileWidth": 256,
   "tileHeight": 192,
